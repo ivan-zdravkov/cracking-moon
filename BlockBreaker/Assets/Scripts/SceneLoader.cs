7ed79ee7e6db01d13682f28ed2d14bb3737f012d
i-z <IvanZdravkovBG@gmail.com>
2019-07-23 10:05:41 +0300

    Setup.

---
+++ BlockBreaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        else
+            SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
