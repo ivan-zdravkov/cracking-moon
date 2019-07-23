5a9105ae3b76a44f207539b45c8a63218bc9e5d7
i-z <IvanZdravkovBG@gmail.com>
2019-07-23 23:29:22 +0300

    Ball and move paddle.

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16;
+
+    void Update()
+    {
+        transform.position = new Vector2(
+            x: Mathf.Clamp(
+                value: Input.mousePosition.x / Screen.width * this.screenWidthInUnits,
+                min: 0,
+                max: this.screenWidthInUnits
+            ),
+            y: transform.position.y
+        );
+    }
+}
