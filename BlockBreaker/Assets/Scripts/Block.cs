5b3c66e3a873204d7627d0d51bfbbfb000e287c9
i-z <IvanZdravkovBG@gmail.com>
2019-07-29 17:15:31 +0300

    Increase Player Score.

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -7,19 +7,28 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip breakSound;
 
     Level level;
+    GameStatus gameStatus;
 
     private void Start()
     {
         this.level = FindObjectOfType<Level>();
+        this.gameStatus = FindObjectOfType<GameStatus>();
+
         this.level.CountBreakableBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
         AudioSource.PlayClipAtPoint(this.breakSound, this.transform.position);
 
         Destroy(this.gameObject);
 
+        this.gameStatus.AddToScore();
         this.level.BlockDestroyed();
     }
 }
---
+++ BlockBreaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlock = 83;
+
+    [SerializeField] int score = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        this.score += this.pointsPerBlock;
+    }
+}
