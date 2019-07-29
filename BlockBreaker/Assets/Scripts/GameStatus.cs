07cc4fe5e0014a0e39b4742b7bfc1f3af770da64
i-z <IvanZdravkovBG@gmail.com>
2019-07-29 19:42:50 +0300

    Persistant Score.

--- BlockBreaker/Assets/Scripts/GameStatus.cs
+++ BlockBreaker/Assets/Scripts/GameStatus.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 public class GameStatus : MonoBehaviour
 {
-
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlock = 83;
     [SerializeField] TextMeshProUGUI scoreText;
 
     [SerializeField] int score = 0;
 
-    private void Start()
+    void Awake()
+    {
+        bool isFirst = FindObjectsOfType<GameStatus>().Length == 1;
+
+        if (isFirst)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            this.gameObject.SetActive(false);
+
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
     {
         this.scoreText.SetText(this.score.ToString());
     }
